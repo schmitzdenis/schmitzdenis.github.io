f96be4d97bb850c029ecbaa4aaa1fe13ce325c06
Denis Schmitz <schmitzdenis@gmail.com>
2015-10-07 19:47:53 +0200

    renames the favico folder

--- frontend/components/gallery.jsx
+++ frontend/components/gallery.jsx
@@ -21,27 +21,51 @@ class Gallery extends React.Component {
           name: 'Susy',
           image: 'susy.png',
           description: 'YOUR MARKUP, YOUR DESIGN, YOUR OPINIONS | OUR MATH.'
+        },{
+          name: 'React',
+          image: 'react.png',
+          description: 'Lots of people use React as the V in MVC. Since React makes no assumptions about the rest of your technology stack, it\'s easy to try it out on a small feature in an existing project.'
+        },{
+          name: 'Sass',
+          image: 'sass.png',
+          description: 'Sass is an extension of CSS3, adding nested rules, variables, mixins, selector inheritance, and more. It\'s translated to well-formatted, standard CSS using the command line tool or a web-framework plugin.'
+        },{
+          name: 'Susy',
+          image: 'susy.png',
+          description: 'YOUR MARKUP, YOUR DESIGN, YOUR OPINIONS | OUR MATH.'
+        },{
+          name: 'React',
+          image: 'react.png',
+          description: 'Lots of people use React as the V in MVC. Since React makes no assumptions about the rest of your technology stack, it\'s easy to try it out on a small feature in an existing project.'
+        },{
+          name: 'Sass',
+          image: 'sass.png',
+          description: 'Sass is an extension of CSS3, adding nested rules, variables, mixins, selector inheritance, and more. It\'s translated to well-formatted, standard CSS using the command line tool or a web-framework plugin.'
+        },{
+          name: 'Susy',
+          image: 'susy.png',
+          description: 'YOUR MARKUP, YOUR DESIGN, YOUR OPINIONS | OUR MATH.'
         }
       ]
     };
   }
 
-//componentDidMount() {
-  //Fetch techs from api and setState
-  /*
-   $.ajax({
-     url: this.props.url,
-     dataType: 'json',
-     cache: false,
-     success: function(data) {
-       this.setState({data: data});
-     }.bind(this),
-     error: function(xhr, status, err) {
-       console.error(this.props.url, status, err.toString());
-     }.bind(this)
-   });
-   */
- //}
+  //componentDidMount() {
+    //Fetch techs from api and setState
+    /*
+     $.ajax({
+       url: this.props.url,
+       dataType: 'json',
+       cache: false,
+       success: function(data) {
+         this.setState({data: data});
+       }.bind(this),
+       error: function(xhr, status, err) {
+         console.error(this.props.url, status, err.toString());
+       }.bind(this)
+     });
+     */
+   //}
 
   render() {
     var techList = this.state.techs.map(function (tech) {
