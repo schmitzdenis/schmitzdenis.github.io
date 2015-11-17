21c0d24f774943c5190781767c1f1f775c219e25
Denis Schmitz <schmitzdenis@gmail.com>
2015-11-18 00:04:22 +0100

    use require form of file-loader

--- entry.jsx
+++ entry.jsx
@@ -1,6 +1,10 @@
+require("file?name=[name].[ext]!./index.html");
+require("file?name=[name]!./CNAME");
+
 import React from 'react';
 import Layout from './components/layout.jsx';
 import css from './components/style.scss';
+
 
 
 React.render(<Layout/>, document.getElementById('content'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,44 @@
 var path = require('path');
 
-module.exports = {
+var config = {
+  addVendor: function(name, path) {
+    this.resolve.alias[name] = path;
+    this.module.noParse.push(new RegExp(path));
+  },
   entry: {
-    javascript : path.join(__dirname, 'entry.jsx'),
-    html: path.join(__dirname, 'index.html')
-    /*cname: path.join(__dirname ,'CNAME')*/
+    javascript: path.join(__dirname, 'entry.jsx'),
+    //html: path.join(__dirname, 'index.html')
+      /*cname: path.join(__dirname ,'CNAME')*/
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   },
   output: {
-    path: path.resolve(__dirname ,'dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   module: {
     loaders: [{
-      test: /\.html$/,
-      exclude: /node_modules/,
-      loader: 'file?name=[name].[ext]'
-    },{
       test: /\.png$/,
       loader: 'url-loader',
-      query: { mimetype: 'image/png' }
-    },{
-      test: /\CNAME$/,
-      exclude: /node_modules/,
-      loader: 'file?name=[name]'
-    },{
+      query: {
+        mimetype: 'image/png'
+      }
+    }, {
       test: /\.scss$/,
       exclude: /node_modules/,
       loader: 'style!css!sass'
-    },{
+    }, {
       test: /\.jsx$/,
       exclude: /node_modules/,
-      loaders: ['react-hot','babel-loader']
-    }]
+      loaders: ['react-hot', 'babel-loader']
+    }],
+    noParse: [
+      'index.html',
+      'CNAME'
+    ],
   },
+
   /*
   externals: {
     //don't bundle the 'react' npm package with our bundle.js
@@ -40,7 +46,8 @@ module.exports = {
     'react': 'React'
   },
   */
-  debug:true,
+
+  debug: true,
   devtool: 'eval-source-map',
   devServer: {
     noInfo: true, //  --no-info option
@@ -49,10 +56,12 @@ module.exports = {
     progress: true,
     watch: true,
     quiet: false,
-    port:3000,
+    port: 3000,
     //https:true,
-    stats: { colors: true }
-  },resolve: {
-    extensions: ['', '.js', '.jsx']
+    stats: {
+      colors: true
+    }
   }
 };
+
+module.exports = config;
